c340e622d7c08bd220aa11044bb96f1eee3deba7
Matias-sh <devmatiasbritez@gmail.com>
2025-01-03 09:22:59 -0300

    pantalla de carga creada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.blenderapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.blenderapp"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
